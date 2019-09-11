0db2bac7d34c6f096444317db415da18262ce1db
Kemalcan Bora <kemalcanbora@gmail.com>
2019-09-11 17:56:44 +0300

    __init__ test

--- install_spark.sh
+++ install_spark.sh
@@ -1,4 +1,6 @@
 apt update
+apt -y install openssh-server
+apt -y apt-get install sshpass
 add-apt-repository ppa:webupd8team/java
 apt -y update
 apt -y install oracle-java8-installer oracle-java8-set-default
@@ -6,10 +8,8 @@ apt-get -y install openssh-server openssh-client
 apt-get -y install scala
 curl -O http://apache.mirror.anlx.net/spark/spark-2.3.4/spark-2.3.4-bin-hadoop2.6.tgz
 tar xvf spark-2.3.4-bin-hadoop2.6.tgz
-mv spark-2.3.4-bin-hadoop2.6/ /opt/spark
+echo 123456 | sudo -S mv spark-2.3.4-bin-hadoop2.6/ /opt/spark
 echo -e "\n" >> ~/.bashrc
 echo '''export SPARK_HOME=/opt/spark''' >> ~/.bashrc
 echo '''export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin''' >> ~/.bashrc
 source ~/.bashrc
-cd /opt/spark
-./sbin/start-all.sh
